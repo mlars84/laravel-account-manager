e3c350d397424ec69a7153ade1da6941466f2a92
Matt Larson <matt.a.larson@gmail.com>
2017-10-25 12:28:23 -0500

    removed activate text

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -17,7 +17,8 @@
             <li>Deactivated: 4</li>
         </ul>
         <input class="inputs" type="number" name="ati" placeholder="Enter Account Type ID">
-        <input class="inputs" type="number" name="active" placeholder="0 for inactive/1 for active"><p>Click to Activate!</p>
+        <input class="inputs" type="number" name="active" placeholder="0 for inactive/1 for active">
+        <!-- <p>Click to Activate!</p> -->
         <button class="button">Submit</button>
       </form>
   </div>
